c6a7ff15dddb6158703855058470a662232d72c1
samirarabbanian <samira.rabanian@gmail.com>
2014-02-18 08:17:47 +0000

    Simplified code and removed unneeded code

--- TestServer.java
+++ TestServer.java
@@ -14,53 +14,38 @@ import java.nio.charset.Charset;
 
 import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_LENGTH;
 import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
-import static io.netty.handler.codec.http.HttpHeaders.is100ContinueExpected;
-import static io.netty.handler.codec.http.HttpResponseStatus.CONTINUE;
 import static io.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
-/**
- * An HTTP server that sends back the content of the received HTTP request
- * in a pretty plaintext form.
- */
+
 public class TestServer {
 
-    private static final NioEventLoopGroup workerGroup = new NioEventLoopGroup();
-    private static final NioEventLoopGroup bossGroup = new NioEventLoopGroup();
-
     public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(new Runnable() {
+        System.out.println("STARTING SERVER FOR HTTP ON PORT: " + 8080);
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            new ServerBootstrap()
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
 
-            @Override
-            public void run() {
-                try {
-                    System.out.println("STARTING SERVER FOR HTTP ON PORT: " + 8080);
-                    new ServerBootstrap()
-                            .option(ChannelOption.SO_BACKLOG, 1024)
-                            .group(bossGroup, workerGroup)
-                            .channel(NioServerSocketChannel.class)
-                            .childHandler(new ChannelInitializer<SocketChannel>() {
-                                @Override
-                                public void initChannel(SocketChannel ch) throws Exception {
-                                    ChannelPipeline pipeline = ch.pipeline();
-
-                                    pipeline.addLast("logger", new LoggingHandler("TEST_SERVER"));
-                                    pipeline.addLast("codec", new HttpServerCodec());
-                                    pipeline.addLast("handler", new TestServerHandler());
-                                }
-                            })
-                            .bind(8080).channel().closeFuture().sync();
-                } catch (Exception e) {
-                    throw new RuntimeException("Exception running test server", e);
-                } finally {
-                    bossGroup.shutdownGracefully();
-                    workerGroup.shutdownGracefully();
-                }
-            }
-        });
-        thread.start();
-        thread.join();
+                            pipeline.addLast("logger", new LoggingHandler("TEST_SERVER"));
+                            pipeline.addLast("http_codec", new HttpServerCodec());
+                            pipeline.addLast("simple_test_handler", new TestServerHandler());
+                        }
+                    })
+                    .bind(8080).channel().closeFuture().sync();
+        } catch (Exception e) {
+            throw new RuntimeException("Exception running test server", e);
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 
     private static class TestServerHandler extends SimpleChannelInboundHandler<Object> {
@@ -75,10 +60,6 @@ public class TestServer {
             if (msg instanceof HttpRequest) {
                 HttpRequest req = (HttpRequest) msg;
 
-                if (is100ContinueExpected(req)) {
-                    ctx.write(new DefaultFullHttpResponse(HTTP_1_1, CONTINUE));
-                }
-
                 FullHttpResponse response;
                 if (req.getUri().equals("/unknown")) {
                     response = new DefaultFullHttpResponse(HTTP_1_1, NOT_FOUND);
@@ -89,7 +70,7 @@ public class TestServer {
                     response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
                 } else if (req.getUri().equals("/netty")) {
                     response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer("it is also fun to play with Netty".getBytes(Charset.forName("UTF-8"))));
-                    response.headers().set("X-ZeroMQ", "a_header_value");
+                    response.headers().set("X-Netty", "a_header_value");
                     response.headers().set(CONTENT_TYPE, "text/plain");
                     response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
                 } else {
